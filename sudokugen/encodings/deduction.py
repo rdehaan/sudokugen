687406f4559fbfba6b5e70f3f8f7dfb1ae1a6654
Ronald de Haan <me@ronalddehaan.eu>
2022-07-15 20:53:54 +0200

    Add deduction rule for *not* deriving a mask.

--- sudokugen/encodings/deduction.py
+++ sudokugen/encodings/deduction.py
@@ -693,7 +693,7 @@ def stable_state_mask_derived(
     ) -> str:
     """
     Given a mask, produce a deduction rule that states that this mask must be
-    derived exactly (in the deduction mode in which the rule is used): if the
+    derived (in the deduction mode in which the rule is used): if the
     mask has a 0 for a cell, no solution may be derived for this cell, if the
     mask has a positive integer, this integer must be derived for this cell,
     if the mask has a "*", some solution must be derived for this cell,
@@ -711,7 +711,7 @@ def stable_state_mask_derived(
         if val == "0":
             asp_code += f"""
                 :- deduction_mode(Mode),
-                    use_technique(Mode,derive_mask_exactly({mask})),
+                    use_technique(Mode,ss_mask_derived({mask})),
                     cell({cell}), value(V),
                     solution({cell},V),
                     derivable(Mode,solution({cell},V)).
@@ -719,7 +719,7 @@ def stable_state_mask_derived(
         elif val == "*":
             asp_code += f"""
                 :- deduction_mode(Mode),
-                    use_technique(Mode,derive_mask_exactly({mask})),
+                    use_technique(Mode,ss_mask_derived({mask})),
                     cell({cell}), value(V),
                     solution({cell},V),
                     not derivable(Mode,solution({cell},V)).
@@ -730,7 +730,7 @@ def stable_state_mask_derived(
                 asp_code += f"""
                     solution({cell},{val}).
                     :- deduction_mode(Mode),
-                        use_technique(Mode,derive_mask_exactly({mask})),
+                        use_technique(Mode,ss_mask_derived({mask})),
                         cell({cell}),
                         solution({cell},{val}),
                         not derivable(Mode,solution({cell},{val})).
@@ -739,7 +739,7 @@ def stable_state_mask_derived(
                 pass
 
     deduction_rule = DeductionRule(
-        f"derive_mask_exactly({mask})",
+        f"ss_mask_derived({mask})",
         asp_code
     )
     return deduction_rule
@@ -749,6 +749,66 @@ def stable_state_mask_not_derived(
         mask: str
     ) -> str:
     """
-    TODO
+    Given a mask, produce a deduction rule that states that this mask must *NOT*
+    be derived (in the deduction mode in which the rule is used). A mask is
+    derived if the following conditions holds for all cells: if the mask
+    mask has a 0 for a cell, no solution may be derived for this cell, if the
+    mask has a positive integer, this integer must be derived for this cell,
+    if the mask has a "*", some solution must be derived for this cell,
+    and if the mask has a "?", no constraints are posed on what may be derived
+    for this cell.
     """
-    pass
+
+    asp_code = ""
+    mask_pieces = [
+        (j, i, mask[(i-1) * instance.size + j - 1])
+        for (i, j) in itertools.product(range(1, instance.size+1), repeat=2)
+    ]
+    for (i, j, val) in mask_pieces:
+        cell = instance.cell_encoding((i,j))
+        if val == "0":
+            asp_code += f"""
+                derivable(Mode,mask_not_derived({cell},{mask})) :-
+                    deduction_mode(Mode),
+                    use_technique(Mode,ss_mask_not_derived({mask})),
+                    cell({cell}), value(V),
+                    solution({cell},V),
+                    derivable(Mode,solution({cell},V)).
+            """
+        elif val == "*":
+            asp_code += f"""
+                derivable(Mode,mask_not_derived({cell},{mask})) :-
+                    deduction_mode(Mode),
+                    use_technique(Mode,ss_mask_not_derived({mask})),
+                    cell({cell}), value(V),
+                    solution({cell},V),
+                    not derivable(Mode,solution({cell},V)).
+            """
+        else:
+            try:
+                val = int(val)
+                asp_code += f"""
+                    derivable(Mode,mask_not_derived({cell},{mask})) :-
+                        deduction_mode(Mode),
+                        use_technique(Mode,ss_mask_not_derived({mask})),
+                        cell({cell}),
+                        not derivable(Mode,solution({cell},{val})).
+                """
+            except ValueError:
+                pass
+
+    asp_code += f"""
+        derivable(Mode,mask_not_derived({mask})) :-
+            deduction_mode(Mode),
+            use_technique(Mode,ss_mask_not_derived({mask})),
+            cell(C), derivable(Mode,mask_not_derived(C,{mask})).
+        :- deduction_mode(Mode),
+            use_technique(Mode,ss_mask_not_derived({mask})),
+            not derivable(Mode,mask_not_derived({mask})).
+    """
+
+    deduction_rule = DeductionRule(
+        f"ss_mask_not_derived({mask})",
+        asp_code
+    )
+    return deduction_rule
