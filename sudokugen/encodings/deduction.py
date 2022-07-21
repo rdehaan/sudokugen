949032ff8fe98598c57bfef20937562b63f40cbd
Ronald de Haan <me@ronalddehaan.eu>
2022-07-21 13:36:30 +0200

    Remove old version of encodings of color wrap and color trap

--- sudokugen/encodings/deduction.py
+++ sudokugen/encodings/deduction.py
@@ -574,56 +574,6 @@ x_chain = DeductionRule(
     # """
 )
 
-# color_trap = DeductionRule(
-#     "color_trap",
-#     """
-#     derivable(Mode,unicolor_iff(C1,yes(V),C2,no(V))) :-
-#         use_technique(Mode,color_trap), deduction_mode(Mode),
-#         value(V), cell(C1), cell(C2), different_cells(C1,C2),
-#         not certainly_not_erased(C1),
-#         not certainly_not_erased(C2),
-#         group(G), active_group(Mode,G),
-#         in_group(C1,G), in_group(C2,G),
-#         derivable(Mode,strike(C,V)) :
-#             cell(C), in_group(C,G),
-#             different_cells(C,C1), different_cells(C,C2).
-#     derivable(Mode,unicolor_iff(C1,S1,C2,S2)) :-
-#         derivable(Mode,unicolor_iff(C2,S2,C1,S1)).
-#     derivable(Mode,unicolor_iff(C1,S1,C3,S3)) :-
-#         use_technique(Mode,color_trap), deduction_mode(Mode),
-#         derivable(Mode,unicolor_iff(C1,S1,C2,S2)),
-#         derivable(Mode,unicolor_iff(C2,S2,C3,S3)),
-#         different_cells(C1,C2), different_cells(C2,C3),
-#         different_cells(C1,C3),
-#         not certainly_not_erased(C1),
-#         not certainly_not_erased(C2),
-#         not certainly_not_erased(C3).
-#     derivable(Mode,strike(C,V)) :-
-#         use_technique(Mode,color_trap), deduction_mode(Mode),
-#         cell(C), not certainly_not_erased(C),
-#         value(V),
-#         derivable(Mode,unicolor_iff(C1,yes(V),C2,no(V))),
-#         share_group(C,C1), share_group(C,C2),
-#         different_cells(C1,C2).
-#
-#     :- derivable(_,unicolor_iff(C1,no(V),C2,no(V))),
-#         not solution(C1,V), solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,no(V),C2,no(V))),
-#         solution(C1,V), not solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,yes(V),C2,yes(V))),
-#         not solution(C1,V), solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,yes(V),C2,yes(V))),
-#         solution(C1,V), not solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,no(V),C2,yes(V))),
-#         not solution(C1,V), not solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,no(V),C2,yes(V))),
-#         solution(C1,V), solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,yes(V),C2,no(V))),
-#         not solution(C1,V), not solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,yes(V),C2,no(V))),
-#         solution(C1,V), solution(C2,V).
-#     """
-# )
 color_trap = DeductionRule(
     "color_trap",
     """
@@ -683,54 +633,6 @@ color_trap = DeductionRule(
     """
 )
 
-# color_wrap = DeductionRule(
-#     "color_wrap",
-#     """
-#     derivable(Mode,unicolor_iff(C1,yes(V),C2,no(V))) :-
-#         use_technique(Mode,color_wrap), deduction_mode(Mode),
-#         value(V), cell(C1), cell(C2), different_cells(C1,C2),
-#         not certainly_not_erased(C1),
-#         not certainly_not_erased(C2),
-#         group(G), active_group(Mode,G),
-#         in_group(C1,G), in_group(C2,G),
-#         derivable(Mode,strike(C,V)) :
-#             cell(C), in_group(C,G),
-#             different_cells(C,C1), different_cells(C,C2).
-#     derivable(Mode,unicolor_iff(C1,S1,C2,S2)) :-
-#         derivable(Mode,unicolor_iff(C2,S2,C1,S1)).
-#     derivable(Mode,unicolor_iff(C1,S1,C3,S3)) :-
-#         use_technique(Mode,color_wrap), deduction_mode(Mode),
-#         derivable(Mode,unicolor_iff(C1,S1,C2,S2)),
-#         derivable(Mode,unicolor_iff(C2,S2,C3,S3)),
-#         different_cells(C1,C2), different_cells(C2,C3),
-#         different_cells(C1,C3),
-#         not certainly_not_erased(C1),
-#         not certainly_not_erased(C2),
-#         not certainly_not_erased(C3).
-#     derivable(Mode,strike(C1,V)) :-
-#         use_technique(Mode,color_wrap), deduction_mode(Mode),
-#         value(V),
-#         derivable(Mode,unicolor_iff(C1,yes(V),C2,yes(V))),
-#         share_group(C1,C2), different_cells(C1,C2).
-#
-#     :- derivable(_,unicolor_iff(C1,no(V),C2,no(V))),
-#         not solution(C1,V), solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,no(V),C2,no(V))),
-#         solution(C1,V), not solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,yes(V),C2,yes(V))),
-#         not solution(C1,V), solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,yes(V),C2,yes(V))),
-#         solution(C1,V), not solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,no(V),C2,yes(V))),
-#         not solution(C1,V), not solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,no(V),C2,yes(V))),
-#         solution(C1,V), solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,yes(V),C2,no(V))),
-#         not solution(C1,V), not solution(C2,V).
-#     :- derivable(_,unicolor_iff(C1,yes(V),C2,no(V))),
-#         solution(C1,V), solution(C2,V).
-#     """
-# )
 color_wrap = DeductionRule(
     "color_wrap",
     """
